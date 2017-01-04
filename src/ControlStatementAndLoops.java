b5009c4f94f78330a5d9d99434054a8dbbc1ac23
V-Rod <pjvelazquez@me.com>
2017-01-04 11:51:18 -0600

    Java I -5.3.1 while, do-while, for loops

---
+++ src/ControlStatementAndLoops.java
@@ -0,0 +1,57 @@
+/**
+ * Created by V-Rod on 1/4/17.
+ */
+public class ControlStatementAndLoops {
+    public static void main(String[] args) {
+
+        //while loop
+        int i = 5;
+        while (i <=15) {
+            System.out.println("i is " + i);
+            i++;
+        }
+
+        //do while loop
+        int count = 0;
+        do {
+            System.out.println("count is " + count);
+            count +=2;
+
+        } while (count <=100);
+
+        int countBackwards = 100;
+        do {
+            System.out.println("reverse count is " + countBackwards);
+            countBackwards -= 5;
+        } while (countBackwards >= -10);
+
+        double countSquared = 2;
+        do {
+            System.out.println("squared count is " + countSquared);
+            countSquared = Math.pow(countSquared, 2);
+        } while (countSquared <= 1000000);
+
+        //refactoring previous exercises with a for loop
+
+        for (int forInt = 5; forInt <= 15; forInt++) {
+            System.out.println("forInt is " + forInt);
+        }
+
+        for (int forCount = 0; forCount <=100; forCount+=2){
+            System.out.println("forCount is " + forCount);
+        }
+
+        for (int forCountBackwards = 100; forCountBackwards >= -10; forCountBackwards -= 5) {
+            System.out.println("forCountBackwards is " + forCountBackwards);
+        }
+
+        for (double forCountSquared = 2; forCountSquared <= 1000000; forCountSquared = Math.pow(forCountSquared, 2)){
+            System.out.println("forCountSquared is " + forCountSquared);
+        }
+
+        System.out.println("Thank you for using this program.");
+
+
+
+    }
+}
