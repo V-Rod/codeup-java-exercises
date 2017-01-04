2a9b96d77a560f2ad87bfd3e99392939f55c9e77
V-Rod <pjvelazquez@me.com>
2017-01-04 12:10:16 -0600

    Java I -5.3.2 FizzBuzz between 1-100 hardcoded

---
+++ src/FizzBuzz.java
@@ -0,0 +1,21 @@
+/**
+ * Created by V-Rod on 1/4/17.
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count < 100) {
+            //System.out.println("count is " + count);
+            count++;
+            if (count % 3 == 0 && count % 5 == 0) {
+                System.out.println("FizzBuzz!");
+            } else if (count % 5 == 0){
+                System.out.println("Buzz!");
+            } else if (count % 3 == 0) {
+                System.out.println("Fizz!");
+            } else {
+                System.out.println(count);
+            }
+        }
+    }
+}
