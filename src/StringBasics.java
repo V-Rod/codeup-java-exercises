9ae8bcbd9e1ce5741edcd8246f17d14dae495c04
V-Rod <pjvelazquez@me.com>
2017-01-04 16:52:56 -0600

    Java I - 5.4.1 string basics

---
+++ src/StringBasics.java
@@ -0,0 +1,15 @@
+/**
+ * Created by V-Rod on 1/4/17.
+ */
+public class StringBasics {
+
+    public static void main(String[] args) {
+        String message = "We don't need no education \nWe don't need no thought control\n";
+
+        message += "\nCheck \"this\" out, \"s inside of \"s\"!\n";
+        message += "In windows, the main drive is usually C:\\\n";
+        message += "I can do back slashes \\, and double back slashes \\\\, and the amazing triple back slash \\\\\\!";
+
+        System.out.println(message);
+    }
+}
