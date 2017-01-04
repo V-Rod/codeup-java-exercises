9d59cd6c7b5f6238b7ef3b437d4f8eb4428550c3
V-Rod <pjvelazquez@me.com>
2017-01-04 14:26:25 -0600

    Java I -5.3.3 Display a table of powers

--- src/TableOfPowers.java
+++ src/TableOfPowers.java
@@ -6,14 +6,40 @@ import java.util.Scanner;
 public class TableOfPowers {
     public static void main(String[] args) {
         Scanner squareCubeTable = new Scanner(System.in);
-        String userInteger;
+        int userInput;
+        String userAnswer;
 
-        System.out.println("Welcome to the Squares and Cubes Table");
-        System.out.println();
+        //Welcome the user to the program
 
-        System.out.print("Please enter an integer: ");
-        userInteger = squareCubeTable.nextLine();
 
+            System.out.println("Welcome to the Squares and Cubes Table.  The table will be able to display a table of " +
+                    "squares and cubes from 1 to the value entered.");
+            System.out.println();
+        do {
+            //Ask for input from user
+            System.out.print("Please enter an integer: ");
+            userInput = squareCubeTable.nextInt();
+
+
+            //Create the table layout
+            System.out.println("Number" + " " + " Squared" + " " + " Cubed");
+            System.out.println("====== " + " " + "=======" + " " + " =====");
+
+            //Iterate over the array
+            for (int i = 1; i <= userInput; i++) {
+                int resultSquared = (int) Math.pow(i, 2);
+                int resultCubed = (int) Math.pow(i, 3);
+                System.out.println(i  + "        " + resultSquared + "       " + resultCubed);
+            }
+
+            System.out.print("Would you like to run the program again?  Type yes or no: ");
+            squareCubeTable.nextLine();
+            userAnswer = squareCubeTable.nextLine();
+
+
+        } while (userAnswer.equals("yes") || userAnswer.equals("y"));
+
+        System.out.println("Thank you!");
 
     }
 }
