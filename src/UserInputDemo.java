c48bd3d4dcfccd131dcf5c9d44518d458a9cc17c
V-Rod <pjvelazquez@me.com>
2017-01-04 10:45:44 -0600

    Java I -5.2 exercise fixed and cleaned

--- src/UserInputDemo.java
+++ src/UserInputDemo.java
@@ -5,73 +5,77 @@ import java.util.Scanner;
  */
 public class UserInputDemo {
     public static void main(String[] args) {
-//        String userInput;
-//
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Please input something: ");
-//        userInput = sc.nextLine();
-//        System.out.println("You enterered " + userInput);
-
-//        Integer userInt;
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Please enter an integer: ");
-//        userInt = sc.nextInt();
-//        System.out.println("You entered " + userInt);
-
-//        String userSentence;
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Input a sentence: ");
-//        userSentence = sc.nextLine();
-//        System.out.print("You entered " + userSentence);
-
-//        String firstInput;
-//        String secondInput;
-//        String thirdInput;
-//
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Please enter a word: ");
-//        firstInput = sc.nextLine();
-//        System.out.print("Your first word is " + firstInput + ". ");
-//
-//        System.out.print("Please enter a second word: ");
-//        secondInput = sc.nextLine();
-//        System.out.print("Your second word is " + secondInput + ". ");
-//
-//        System.out.print("Please enter a third word: ");
-//        thirdInput = sc.nextLine();
-//        System.out.print("Your third word is " + thirdInput + ".");
-
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+        Integer userInt;
+        String userSentence;
+        String firstInput;
+        String secondInput;
+        String thirdInput;
         Double length;
         Double width;
         Double height;
-//
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter the room's length: ");
-//        length = sc.nextDouble();
-//        System.out.print("Enter the room's width: ");
-//        width = sc.nextDouble();
-//        System.out.print("The perimeter of the room is: " + 2 * (length + width));
+        String userAnswer;
 
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter the room's length: ");
-//        length = sc.nextDouble();
-//        System.out.print("Enter the room's width: ");
-//        width = sc.nextDouble();
-//        System.out.print("The area of the room is: " + (length * width) + " feet.");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the room's length: ");
-        length = sc.nextDouble();
-        System.out.print("Enter the room's width: ");
-        width = sc.nextDouble();
-        System.out.print("Enter the room's height: ");
-        height = sc.nextDouble();
-        System.out.print("The volume of the room is: " + (length * width * height) + ". ");
+        do {
+            System.out.print("Please input something: ");
+            userInput = sc.nextLine();
+            System.out.println("You enterered " + userInput);
 
-        String userInput;
+            System.out.print("Please enter an integer: ");
+            userInt = sc.nextInt();
+            System.out.println("You entered " + userInt);
 
-        System.out.print("Would you like to continue?  Type yes or no: ");
-        userInput = sc.nextLine();
+            //Make sure to not skip the next input
+            sc.nextLine();
+
+
+            System.out.print("Input a sentence: ");
+            userSentence = sc.nextLine();
+            System.out.println("You entered " + userSentence + ".");
+
+
+            System.out.print("Please enter a word: ");
+            firstInput = sc.next();
+            System.out.println("Your first word is " + firstInput + ". ");
+
+            System.out.print("Please enter a second word: ");
+            secondInput = sc.next();
+            System.out.println("Your second word is " + secondInput + ". ");
+
+            System.out.print("Please enter a third word: ");
+            thirdInput = sc.next();
+            System.out.println("Your third word is " + thirdInput + ". ");
+
+
+            System.out.println("Next, we will calculate the area, perimeter, and volumen of a room. ");
+
+
+            System.out.print("Enter the room's length: ");
+            length = sc.nextDouble();
+            System.out.print("Enter the room's width: ");
+            width = sc.nextDouble();
+            System.out.print("Enter the room's height: ");
+            height = sc.nextDouble();
+
+            System.out.println("The perimeter of the room is: " + 2 * (length + width) + ". ");
+
+            System.out.println("The area of the room is: " + (length * width) + " feet. ");
+
+            System.out.println("The volume of the room is: " + (length * width * height) + ". ");
+
+            //Make sure to not skip the next input
+            sc.nextLine();
+
+
+            System.out.print("Would you like to run the program again?  Type yes or no: ");
+            userAnswer = sc.nextLine();
+
+        } while (userAnswer.equals("yes") || userAnswer.equals("y"));
+
+        System.out.println("Thank you!");
+
 
     }
 }
