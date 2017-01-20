22c8f5804a63f92e8f4fede64b620c93e3fd798d
V-Rod <pjvelazquez@me.com>
2017-01-20 14:28:00 -0600

    Java I - Roshambo exercise (refactored code for HumanPlayer and Player)

--- src/Player.java
+++ src/Player.java
@@ -5,6 +5,8 @@ public abstract class Player {
 
     protected String name;
     protected String response;
+    private int winsCounter = 0;
+    private int lossCounter = 0;
 
     public Player() {
         this.name = getName();
@@ -26,6 +28,35 @@ public abstract class Player {
     public void setResponse(String response) {
         this.response = response;
     }
+
+    public String summary() {
+        return name + " has won " + winsCounter + " times and has lost " + lossCounter + " times.";
+    }
+
+    public void lose() {
+        lossCounter++;
+    }
+
+    public void win() {
+        winsCounter++;
+    }
+
+    public int getWinsCounter() {
+        return winsCounter;
+    }
+
+    public void setWinsCounter(int winsCounter) {
+        this.winsCounter = winsCounter;
+    }
+
+    public int getLossCounter() {
+        return lossCounter;
+    }
+
+    public void setLossCounter(int lossCounter) {
+        this.lossCounter = lossCounter;
+    }
+
 
     //abstract method that allows an inheriting class
     //to generate and return a Roshambo value
--- src/RockPaperScissorsApp.java
+++ src/RockPaperScissorsApp.java
@@ -27,9 +27,10 @@ public class RockPaperScissorsApp {
         System.out.println("You will be playing against one of two computer generated players: Joseph and Herbert.");
         System.out.println();
 
-        // Create the Opponent objects
+        // Create the Opponent objects and HumanPlayer Object
         RockPlayer h = new RockPlayer();
         ComputerPlayer j = new ComputerPlayer();
+        HumanPlayer humanPlayer = new HumanPlayer();
 
         // Input name of human player and ask who does the human player want to play against
         name = Validator.getUserString("Enter your name: ");
@@ -44,48 +45,62 @@ public class RockPaperScissorsApp {
             cpuPlayer = h;
         }
 
-        while(choice.compareTo("y") == 0) {
+        while (choice.compareTo("y") == 0) {
 
             System.out.print("Rock, paper, or scissors (R/P/S): ");
             userRoshambo = sc.next();
-            HumanPlayer humanPlayer = new HumanPlayer();
             humanPlayer.setName(name);
             humanPlayer.setResponse(userRoshambo);
 
             //set Rock for r|R ,Paper for p|P, Scissors for s|S for human player
 
-            if(userRoshambo.charAt(0)=='r' || userRoshambo.charAt(0) == 'R') {
-                userRoshambo="Rock";
-            } else if(userRoshambo.charAt(0)=='p' || userRoshambo.charAt(0) == 'P') {
-                userRoshambo="Paper";
-            } else if(userRoshambo.charAt(0)=='s' || userRoshambo.charAt(0) == 'S') {
-                userRoshambo="Scissors";
+            if (userRoshambo.charAt(0) == 'r' || userRoshambo.charAt(0) == 'R') {
+                userRoshambo = "Rock";
+            } else if (userRoshambo.charAt(0) == 'p' || userRoshambo.charAt(0) == 'P') {
+                userRoshambo = "Paper";
+            } else if (userRoshambo.charAt(0) == 's' || userRoshambo.charAt(0) == 'S') {
+                userRoshambo = "Scissors";
             }
             System.out.println(name + " : " + userRoshambo);
 
             //main logic generate random number for computer player
             compRoshambo = cpuPlayer.generateRoshambo().toString(); //generate opponent's roshambo
-            System.out.println(cpuPlayer.getName()+ " : " + compRoshambo );
+            System.out.println(cpuPlayer.getName() + " : " + compRoshambo);
 
             //match generated random number to roshambo entered by user for all cases
-            if(Character.toUpperCase(userRoshambo.charAt(0)) == compRoshambo.charAt(0)){
+            if (Character.toUpperCase(userRoshambo.charAt(0)) == compRoshambo.charAt(0)) {
                 System.out.println("Draw!");
-            } else if(userRoshambo.equals("Paper") && compRoshambo.equals("Rock")) {
+            } else if (userRoshambo.equals("Paper") && compRoshambo.equals("Rock")) {
                 System.out.println(humanPlayer.getName() + " Wins");
-            } else if(compRoshambo.equals("Paper") && userRoshambo.equals("Rock")) {
+                humanPlayer.win();
+                cpuPlayer.lose();
+            } else if (compRoshambo.equals("Paper") && userRoshambo.equals("Rock")) {
                 System.out.println(cpuPlayer.getName() + " Wins");
-            } else if(userRoshambo.equals("Paper") && compRoshambo.equals("Scissors")) {
+                cpuPlayer.win();
+                humanPlayer.lose();
+            } else if (userRoshambo.equals("Paper") && compRoshambo.equals("Scissors")) {
                 System.out.println(cpuPlayer.getName() + " Wins!");
-            } else if(compRoshambo.equals("Paper") && userRoshambo.equals("Scissors")) {
+                cpuPlayer.win();
+                humanPlayer.lose();
+            } else if (compRoshambo.equals("Paper") && userRoshambo.equals("Scissors")) {
                 System.out.println(humanPlayer.getName() + " Wins!");
-            } else if(userRoshambo.equals("Scissors") && compRoshambo.equals("Rock")) {
+                humanPlayer.win();
+                cpuPlayer.lose();
+            } else if (userRoshambo.equals("Scissors") && compRoshambo.equals("Rock")) {
                 System.out.println(cpuPlayer.getName() + " Wins");
-            } else if(compRoshambo.equals("Scissors") && userRoshambo.equals("Rock")) {
+                cpuPlayer.win();
+                humanPlayer.lose();
+            } else if (compRoshambo.equals("Scissors") && userRoshambo.equals("Rock")) {
                 System.out.println(humanPlayer.getName() + " Wins");
+                humanPlayer.win();
+                cpuPlayer.lose();
             }
             // see if the user wants to continue
             choice = Validator.getString("Play again?(y/n): ");
-            System.out.println();
-        }System.out.println("Thank you for playing!");
+        }
+        System.out.println();
+        System.out.println("Thank you for playing!");
+        System.out.println(humanPlayer.summary());
+        System.out.println(cpuPlayer.summary());
     }
 }
